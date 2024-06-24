be7d2c67690ffc84c09a201e23982a62f23a824e
Nir Galon <nir@galons.io>
2024-06-24 10:11:59 +0300

    chore(app): initial commit by Nuxt Studio

---
+++ nuxt.config.ts
@@ -0,0 +1,49 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: [process.env.NUXT_UI_PRO_PATH || '@nuxt/ui-pro'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/fonts',
+    '@nuxthq/studio',
+    '@vueuse/nuxt',
+    'nuxt-og-image'
+  ],
+  hooks: {
+    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+    'components:extend': (components) => {
+      const globals = components.filter(c => ['UButton'].includes(c.pascalName))
+
+      globals.forEach(c => c.global = true)
+    }
+  },
+  ui: {
+    icons: ['heroicons', 'simple-icons']
+  },
+  colorMode: {
+    disableTransition: true
+  },
+  routeRules: {
+    '/api/search.json': { prerender: true },
+    '/docs': { redirect: '/docs/getting-started', prerender: false }
+  },
+  devtools: {
+    enabled: true
+  },
+  typescript: {
+    strict: false
+  },
+  future: {
+    compatibilityVersion: 4
+  },
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
